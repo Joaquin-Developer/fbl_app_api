7bdeedc9fa9ee2e8c0d2321b8546a386794038d8
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2024-10-22 20:09:41 -0300

    BA-1979 - docker

---
+++ docker_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+docker build -t fbl-app-api .
+
+docker run -d --name fbl-app-api-container --network my_network -p 8007:8007 fbl-app-api
+
+
+#!/bin/bash
+
+# Variables
+IMAGE_NAME="fbl-app-api"
+CONTAINER_NAME="fbl-app-api-container"
+HOST_PORT=8007
+CONTAINER_PORT=8007
+DOCKER_NETWORK="my_network"
+
+echo "Building Docker image..."
+docker build -t $IMAGE_NAME .
+
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    echo "Remove existing container..."
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+fi
+
+echo "Running container..."
+docker run -d --name $CONTAINER_NAME --network $DOCKER_NETWORK -p $HOST_PORT:$CONTAINER_PORT $IMAGE_NAME
+
+echo "Logs:"
+docker logs -f $CONTAINER_NAME
